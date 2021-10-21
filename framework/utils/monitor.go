7b40b40a46409eec8f805a3d32147211fa8e25d5
Alvin <alvin@lavorus.com>
2021-10-21 16:14:59 +0700

    Move log file to folder

--- framework/utils/monitor.go
+++ framework/utils/monitor.go
@@ -7,7 +7,9 @@ import (
 )
 
 func UseMonitor() {
-	file, e := os.OpenFile("monitor.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	_ = os.MkdirAll("log", os.ModePerm)
+
+	file, e := os.OpenFile("log/monitor.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if e != nil {
 		log.Fatalln("Failed to open log file")
 	}
