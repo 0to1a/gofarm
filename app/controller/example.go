789a57bd79215025c1e359cc2f1f9d4fe0e59a03
Alvin <alvin@lavorus.com>
2021-10-22 16:15:32 +0700

    Remove ambiguity context and ctx from webserver

--- app/controller/example.go
+++ app/controller/example.go
@@ -8,35 +8,35 @@ import (
 	"net/http"
 )
 
-func AppTestExample(context echo.Context) error {
-	return webserver.ResultAPI(context, 200, "OK", "")
+func AppTestExample(ctx echo.Context) error {
+	return webserver.ResultAPI(ctx, 200, "OK", "")
 }
 
-func AppLoginExample(context echo.Context) error {
+func AppLoginExample(ctx echo.Context) error {
 	type inputJSON struct {
 		Apikey string `json:"api_key"`
 	}
 
 	inJSON := new(inputJSON)
-	if err := context.Bind(inJSON); err != nil {
-		_ = webserver.ResultAPI(context, http.StatusBadRequest, "Input not valid", "json")
+	if err := ctx.Bind(inJSON); err != nil {
+		_ = webserver.ResultAPI(ctx, http.StatusBadRequest, "Input not valid", "json")
 		return fmt.Errorf("400:nv")
 	}
 
 	id, username := model.ExampleGetApikey(inJSON.Apikey)
 	if id == nil {
-		_ = webserver.ResultAPI(context, http.StatusUnauthorized, "Not Authorized", "api_key")
+		_ = webserver.ResultAPI(ctx, http.StatusUnauthorized, "Not Authorized", "api_key")
 		return fmt.Errorf("401:na")
 	}
 
 	token, err := webserver.JWTCreateToken(username, 15)
 	if err != nil {
-		_ = webserver.ResultAPI(context, http.StatusInternalServerError, "Server Error", "")
+		_ = webserver.ResultAPI(ctx, http.StatusInternalServerError, "Server Error", "")
 		return fmt.Errorf("500:se")
 	}
-	return webserver.ResultAPI(context, 200, "OK", token)
+	return webserver.ResultAPI(ctx, 200, "OK", token)
 }
 
-func AppTestHello(context echo.Context) error {
-	return webserver.ResultAPI(context, 200, "OK", context.Get("username"))
+func AppTestHello(ctx echo.Context) error {
+	return webserver.ResultAPI(ctx, 200, "OK", ctx.Get("username"))
 }
