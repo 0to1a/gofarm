7d2cabac1acfe7bd6ade11859a3b01bcaa75a4ae
Alvin <alvin@lavorus.com>
2022-10-07 12:53:33 +0700

    Hotfix on floating env

--- app/calculateModule/service.go
+++ app/calculateModule/service.go
@@ -1,5 +1,7 @@
 package calculateModule
 
+import "framework/app/structure"
+
 func (w *serviceStructure) plus(n1, n2 int64) int64 {
 	if n1 > 99 || n2 > 99 {
 		return -1
@@ -22,6 +24,8 @@ func (w *serviceStructure) multiple(n1, n2 int64) int64 {
 	for i := int64(0); i < n2; i++ {
 		total += w.plus(total, n1)
 	}
+
+	total = int64(float64(total) * structure.SystemConf.SampleMultiplier)
 
 	return total
 }
